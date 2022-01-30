472c7f5337547b1d09347cb34be74e2cad3b3fad
MathSouz <mmedeiros.cbp@gmail.com>
2022-01-29 21:17:42 -0300

    changed login method to post

--- src/routes/public/user.js
+++ src/routes/public/user.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { login, register } = require("../../controller/user");
 
-router.get("/login", login).post("/register", register);
+router.post("/login", login).post("/register", register);
 
 module.exports = router;
